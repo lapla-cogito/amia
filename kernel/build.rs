db3b53be173cc734de9fc5d8a27f344810f04d4c
lapla-cogito <kakikei20021108@gmail.com>
2024-06-30 01:29:03 +0900

    use include_bytes for shell

--- kernel/build.rs
+++ kernel/build.rs
@@ -2,6 +2,4 @@ fn main() {
     println!("cargo:rerun-if-changed=kernel.ld");
     println!("cargo::rustc-link-arg=-Tkernel.ld");
     println!("cargo::rustc-link-arg=-Map=kernel.map");
-    println!("cargo:rerun-if-changed=shell.bin.o");
-    println!("cargo::rustc-link-arg=shell.bin.o");
 }
